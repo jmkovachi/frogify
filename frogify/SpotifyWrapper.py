2ec635ddaf530e4cca18b8464e4757078d087c29
John Michael Kovachi <johnm.kovachi@gmail.com>
2018-02-10 20:03:41 -0500

    Abstracted login and api requests

--- frogify/SpotifyWrapper.py
+++ frogify/SpotifyWrapper.py
@@ -1,6 +1,10 @@
 import requests
+import json
+import base64
+import urllib
 
-REDIRECT_URI = 'http://localhost:8000/frogify/queue'
+REDIRECT_URI = 'http://localhost:8000/frogify/redirect_login'
+SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
 
 def get_id_and_secret():
     """
@@ -21,26 +25,45 @@ def get_id_and_secret():
 CLIENT_ID, CLIENT_SECRET = get_id_and_secret()
 
 
-def SpotifyWrapper():
+class SpotifyWrapper:
 
+    @staticmethod
+    def get_access_token(auth_code):
+        print('code ' + auth_code)
+        code_payload = {
+            "grant_type": "authorization_code",
+            "code": str(auth_code),
+            "redirect_uri": REDIRECT_URI
+        }
+        data_str = "{}:{}".format(CLIENT_ID, CLIENT_SECRET)
 
-	@staticmethod
-	def get_access_token(auth_code):
-		code_payload = {
-	        "grant_type": "authorization_code",
-	        "code": str(auth_code),
-	        "redirect_uri": REDIRECT_URI
-    	}
+        b64_auth_str = base64.urlsafe_b64encode(data_str.encode()).decode()
 
-	@staticmethod
-	def start_playback(uris=[], auth_header={}):
-		requests.put('https://api.spotify.com/v1/me/player/play', data=json.dumps({'uris' : uris}), headers=auth_header)
+        headers = {"Authorization": "Basic {}".format(b64_auth_str)}
+        post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload, headers=headers)
 
-	@staticmethod
-	def get_playlist_tracks(href='', auth_header={}):
-		playlist_endpoint = '{}/tracks'.format(href)
-		return json.loads(requests.get(playlist_endpoint, headers=auth_header).text)['items']
+        response_data = json.loads(post_request.text)
 
-	@staticmethod
-	def get_user_playlists(username='', auth_header={}):
-		return json.loads(requests.get('https://api.spotify.com/v1/users/{}/playlists'.format(username), headers=auth_header).text)['items']
+        print(response_data)
+
+        access_token = response_data["access_token"]
+        refresh_token = response_data["refresh_token"]
+        token_type = response_data["token_type"]
+        expires_in = response_data["expires_in"]
+
+        return access_token
+
+    @staticmethod
+    def start_playback(uris=[], auth_header={}):
+        requests.put('https://api.spotify.com/v1/me/player/play', data=json.dumps({'uris' : uris}), headers=auth_header)
+
+    @staticmethod
+    def get_playlist_tracks(href='', auth_header={}):
+        playlist_endpoint = '{}/tracks'.format(href)
+        return json.loads(requests.get(playlist_endpoint, headers=auth_header).text)['items']
+
+    @staticmethod
+    def get_user_playlists(username='', auth_header={}):
+        playlists = requests.get('https://api.spotify.com/v1/users/{}/playlists'.format(username), headers=auth_header).text
+        print(playlists)
+        return json.loads(playlists)['items']
--- frogify/urls.py
+++ frogify/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.login, name='login'),
     path('queue', views.queue, name='queue'),
+    path('redirect_login', views.redirect_login, name='redirect_login')
 ]
